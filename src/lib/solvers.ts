78d65d214c26589bc9fe247aa66d6329265b81bc
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-08-08 04:19:45 +0000

    add very complex test case

--- src/lib/solvers.ts
+++ src/lib/solvers.ts
@@ -12,9 +12,8 @@ const extractName: ExtractName = (tuple) => tuple[0];
 type ExtractRemainQuantity = (tuple: NameQuantityTuple) => number;
 const extractRemainQuantity: ExtractRemainQuantity = (tuple) => tuple[1];
 
-type FetchLastElement<T extends any> = (array: T[]) => T;
-const fetchLastElement: FetchLastElement<any> = (array) =>
-  array[array.length - 1];
+type FetchLastElement = <T extends any>(array: T[]) => T;
+const fetchLastElement: FetchLastElement = (array) => array[array.length - 1];
 
 type SortByRemainQuantity = (
   tuples: NameQuantityTuple[]
@@ -22,15 +21,26 @@ type SortByRemainQuantity = (
 const sortByRemainQuantity: SortByRemainQuantity = (tuples) =>
   tuples.sort((a, b) => b[1] - a[1]);
 
+type UniqArray = <T extends any>(array: T[]) => T[];
+const uniqArray: UniqArray = (array) =>
+  array.filter((v, i, a) => a.indexOf(v) === i);
+
+type Compact = <T extends any>(array: T[]) => T[];
+const compact: Compact = (array) => array.filter((elm) => elm != null);
+
 export const stepSolver: StepSolver = ({ nameQuantityTuples }) => {
   const steps: VoteStep[] = [];
 
   while (nameQuantityTuples.length > 0) {
     const sotedTuples = sortByRemainQuantity(nameQuantityTuples);
-    const quantityToDivide = extractRemainQuantity(
-      fetchLastElement(sotedTuples)
-    );
     const dividedTuples = sotedTuples.slice(0, 5);
+    const quantityToDivide = fetchLastElement(
+      compact(
+        uniqArray<number>(sotedTuples.map(extractRemainQuantity))
+          .sort((a, b) => b - a)
+          .slice(0, 5)
+      )
+    );
 
     const dividedNames = dividedTuples.map(extractName);
     steps.push([
