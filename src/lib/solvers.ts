e2bf91d4df02a3fd9778254262243aa06c44f1db
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-08-08 11:26:30 +0000

    refactor to recursive function

--- src/lib/solvers.ts
+++ src/lib/solvers.ts
@@ -28,40 +28,60 @@ const uniqArray: UniqArray = (array) =>
 type Compact = <T extends any>(array: T[]) => T[];
 const compact: Compact = (array) => array.filter((elm) => elm != null);
 
-export const stepSolver: StepSolver = ({ nameQuantityTuples }) => {
-  const steps: VoteStep[] = [];
-  const nameOrder = sortByRemainQuantity(nameQuantityTuples).map(extractName);
-
-  while (nameQuantityTuples.length > 0) {
-    const sotedTuples = sortByRemainQuantity(nameQuantityTuples);
-    const dividedTuples = sotedTuples.slice(0, 5);
-    const quantityToDivide = fetchLastElement(
-      compact(
-        uniqArray<number>(sotedTuples.map(extractRemainQuantity))
-          .sort((a, b) => b - a)
-          .slice(0, 5)
-      )
-    );
-
-    const dividedNames = dividedTuples
-      .map(extractName)
-      .sort((a, b) => nameOrder.indexOf(a) - nameOrder.indexOf(b));
-
-    steps.push([
-      dividedNames[0],
-      dividedNames[1],
-      dividedNames[2],
-      dividedNames[3],
-      dividedNames[4],
-      quantityToDivide,
-    ]);
-
-    dividedTuples.slice(0, 5).forEach((tpl) => (tpl[1] -= quantityToDivide));
-
-    nameQuantityTuples = nameQuantityTuples.filter(
-      (tpl) => extractRemainQuantity(tpl) != 0
-    );
+type SolveByGreedyMethod = (input: {
+  remain: NameQuantityTuple[];
+  steps: VoteStep[];
+  nameOrder: Name[];
+}) => VoteStep[];
+const solveByGrddeyMethod: SolveByGreedyMethod = ({
+  remain,
+  steps,
+  nameOrder,
+}) => {
+  if (remain.length == 0) {
+    return steps;
   }
+
+  const sotedTuples = sortByRemainQuantity(remain);
+  const dividedTuples = sotedTuples.slice(0, 5);
+  const quantityToDivide = fetchLastElement(
+    compact(
+      uniqArray<number>(sotedTuples.map(extractRemainQuantity))
+        .sort((a, b) => b - a)
+        .slice(0, 5)
+    )
+  );
+
+  const dividedNames = dividedTuples
+    .map(extractName)
+    .sort((a, b) => nameOrder.indexOf(a) - nameOrder.indexOf(b));
+
+  steps.push([
+    dividedNames[0],
+    dividedNames[1],
+    dividedNames[2],
+    dividedNames[3],
+    dividedNames[4],
+    quantityToDivide,
+  ]);
+
+  dividedTuples.slice(0, 5).forEach((tpl) => (tpl[1] -= quantityToDivide));
+
+  const nextRemain = remain.filter((tpl) => extractRemainQuantity(tpl) != 0);
+
+  return solveByGrddeyMethod({
+    remain: nextRemain,
+    steps,
+    nameOrder,
+  });
+};
+
+export const stepSolver: StepSolver = ({ nameQuantityTuples }) => {
+  const steps = solveByGrddeyMethod({
+    remain: nameQuantityTuples,
+    steps: [],
+    nameOrder: sortByRemainQuantity(nameQuantityTuples).map(extractName),
+  });
 
   return steps;
 };
